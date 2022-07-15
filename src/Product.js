4342a64b64db2599ffeee539916cbf3a06ec3aa9
Anubha13kumari <anubhamuz@gmail.com>
2022-07-15 16:49:40 +0530

    first page done

--- src/Product.js
+++ src/Product.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import './product.css'
 
-function Product() {
+function Product({title,image,price,rating}) {
   return (
     <div className='product'>
         <div className='product__info'>
-            <p>The Lean startup</p>
+            <p>{title}</p>
             <p className='product__price'>
                 <small>$</small>
-                <strong>19.99</strong>
+                <strong>{price}</strong>
             </p>
             <div className='product__rating'>
-                <p>⭐</p><p>⭐</p><p>⭐</p><p>⭐</p>
-            </div>
+              {Array(rating)
+              .fill()
+              .map((_, i) => (
+                <p>⭐</p>
+              ))}
+               </div>
         </div>
-        <img src='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg' alt=''></img>
+        <img src={image}alt=''></img>
             <button>Add to basket</button>
     </div>
   )
