751edaade72c3cde41cf378526529d882fd143fb
Anubha13kumari <anubhamuz@gmail.com>
2022-07-22 14:48:22 +0530

    all done

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBzrI1FUAfoFWxWG9-mSHlt4-MXW03gsLM",
-    authDomain: "clone-4f7d0.firebaseapp.com",
-    projectId: "clone-4f7d0",
-    storageBucket: "clone-4f7d0.appspot.com",
-    messagingSenderId: "655745677129",
-    appId: "1:655745677129:web:15cd95203e3d20b55e0c98",
-    measurementId: "G-3RKM6J6JPT"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId:  process.env.REACT_APP_APIID,
+    measurementId:process.env.REACT_APP_MEASUREMENTID
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
